c7f1033ce61429a49c950d461e06d1e79e16efea
AJ Bond <olishmollie@gmail.com>
2017-11-08 13:08:45 -0600

    Cleanup

--- main.c
+++ main.c
@@ -4,16 +4,17 @@
 #include <string.h>
 #include <time.h>
 
-time_t start_timer(char*);
-time_t stop_timer(char*);
-int is_running(char*);
+void print_report(time_t);
 
-char *time_str(time_t);
-
+char *create_directory();
+int start_timer(void);
+int stop_timer(void);
+int is_running(void);
 void unknown_command(char*);
 void print_usage(void);
 
 const unsigned long MAXBUFSIZE = 500;
+static char filename[MAXBUFSIZE];
 
 int main(int argc, char *argv[])
 {
@@ -23,100 +24,113 @@ int main(int argc, char *argv[])
     }
 
     unsigned long start_time;
-    char *command, *homedir, *dirname, *filename;
+    char *command;
 
     command = argv[1];
 
-    /* Create directory if doesn't exist */
-    homedir = getenv("HOME");
-    dirname = malloc(MAXBUFSIZE*sizeof(char));
-    snprintf(dirname, MAXBUFSIZE, "%s/.timer", homedir);
-    mkdir(dirname, 0777);
-    free(dirname);
+    char *dirname = create_directory();
+    if (!dirname) {
+	printf("fatal: unable to create '~/.timer' directory\n");
+	exit(1);
+    }
 
-    filename = malloc(MAXBUFSIZE*sizeof(char));
     snprintf(filename, MAXBUFSIZE, "%s/start.tm", dirname);
 
+    int success = 0;
     if (strcmp(command, "start") == 0) {
-	start_time = start_timer(filename);
-	printf("timer begun\n");
+	success = start_timer();
     } else if (strcmp(command, "stop") == 0) {
-	start_time = stop_timer(filename);
-	time_t ft = time(NULL) - start_time;
-	printf("timer stopped\n");
-	printf("time: %s\n", time_str(ft));
-	printf("hours: %.2f\n", ft/3600.0);
+	success = stop_timer();
     } else {
 	unknown_command(command);
     }
 
-    free(filename);
-
-    return 0;
+    return success;
 }
 
-time_t start_timer(char *filename)
+void print_report(time_t start_time)
 {
-    if (!is_running(filename)) {
+    char buf[9];
+
+    time_t final = time(NULL) - start_time;
+    double hrs = final / 3600.0;
+
+    int s = final % 60;
+    int m = final / 60;
+    int h = final / 3600;
+
+    char sec[3], min[3], hr[3];
+
+    snprintf(sec, (size_t)3, (s < 10) ? "0%d" : "%d", s);
+    snprintf(min, (size_t)3, (m < 10) ? "0%d" : "%d", m);
+    snprintf(hr,  (size_t)3, (h < 10) ? "0%d" : "%d", h);
+
+    snprintf(buf, (size_t)9, "%s:%s:%s", hr, min, sec);
+
+    printf("\n");
+    printf("time: \t%s\n", buf);
+    printf("hours:\t%.2f\n", hrs);
+}
+
+char *create_directory()
+{
+    /* TODO: mkdir error handling - return NULL? */
+    char *homedir = malloc(MAXBUFSIZE*sizeof(char));
+    homedir = getenv("HOME");
+    char *dirname = malloc(MAXBUFSIZE*sizeof(char));
+    snprintf(dirname, MAXBUFSIZE, "%s/.timer", homedir);
+    mkdir(dirname, 0777);
+    return dirname;
+}
+
+int start_timer()
+{
+    if (!is_running()) {
 	FILE *f;
 	f = fopen(filename, "w");
 	if (!f) {
-	    printf("fatal: unable to open file\n");
-	    free(filename);
-	    exit(1);
+	    printf("fatal: unable to open %s\n", filename);
+	    return 0;
 	}
 	time_t t = time(NULL);
 	fprintf(f, "%lu\n", t);
 	fclose(f);
-	return t;
+	return 1;
     } else {
 	printf("timer is running. ");
 	printf("run 'timer stop' to stop timer\n");
-	free(filename);
-	exit(1);
+	return 0;
     }
 }
 
-time_t stop_timer(char *filename)
+int stop_timer()
 {
-    if (is_running(filename)) {
+    if (is_running()) {
 	FILE *f;
 	f = fopen(filename, "r");
 	if (!f) {
-	    printf("fatal: unable to open file\n");
-	    free(filename);
-	    exit(1);
+	    printf("fatal: unable to stop timer\n");
+	    return 0;
 	}
 	time_t t;
 	fscanf(f, "%lu", &t);
 	fclose(f);
 	remove(filename);
-	return t;
+	print_report(t);
+	return 1;
     } else {
-	printf("timer never started. ");
 	printf("run 'timer start' to start timer\n");
-	free(filename);
-	exit(1);
+	return 0;
     }
 }
 
-int is_running(char *filename)
+int is_running()
 {
     int result;
     FILE *f = fopen(filename, "r");
     result = f ? 1 : 0;
     fclose(f);
     return result;
-}
-
-char *time_str(time_t t)
-{
-    char *buf = malloc(MAXBUFSIZE*sizeof(char));
-    int sec = t % 60;
-    int min = t / 60;
-    int hr = t / 3600;
-    snprintf(buf, (size_t)MAXBUFSIZE, "%d:%d:%d", hr, min, sec);
-    return buf;
 }
 
 void unknown_command(char* arg)
